5a93d248b421a22624e3575ffe8cbf801a0d8dcb
Roykhan Hidayat <94586935+Hajieping08@users.noreply.github.com>
2022-01-11 13:36:22 +0700

    app.js
    filter our get = routes= app.js
    Tugas Pemograman WEB
    Modul Praktikum 1

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const express = require("express"); 
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const shopRoutes = require("./routes/shop.routes.js");
+const productRoutes = require("./routes/product.routes.js");
 const app = express();
-const server = http.createServer(app); server.listen(5000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/product", productRoutes); app.use(shopRoutes);
+app.listen(5000);
